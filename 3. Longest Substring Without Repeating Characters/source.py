f17beb7380779cf6a561a8f163869c9160c4f25a
deeptoo2 <deeptoo2@naver.com>
2022-05-21 23:57:55 +0900

    Algorithm1 Day6 Sliding Window Q3.

---
+++ 3. Longest Substring Without Repeating Characters/source.py
@@ -0,0 +1,16 @@
+
+
+
+
+import enum
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        dic, res, start, = {}, 0, 0
+        for i, ch in enumerate(s):
+            if ch in dic:
+                res = max(res, i-start)
+                start = max(start, dic[ch]+1)
+            dic[ch] = i
+        return max(res, len(s)-start)
