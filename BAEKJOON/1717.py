6ee002c971e38c8fee247d37459667e0a351a55f
deeptoo2 <deeptoo2@naver.com>
2022-12-24 19:51:07 +0900

    1717

---
+++ BAEKJOON/1717.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+
+n, m = map(int, input().split())
+parent = [i for i in range(n+1)]
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+for i in range(m):
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union_parent(parent, a, b)
+    else:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print("YES")
+        else:
+            print("NO")
+
