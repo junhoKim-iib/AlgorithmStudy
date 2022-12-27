bc5b0e344052323b81ccca252cee348b7d082f82
deeptoo2 <deeptoo2@naver.com>
2022-12-27 18:51:07 +0900

    1929

---
+++ BAEKJOON/1929.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+
+aristo = [ i for i in range(m+1) ]
+
+for i in range(2, m):
+    j = i * i
+    while j <= m:
+       aristo[j] = 0
+       j += i
+
+# drop 0 in the aristo[n:m] list
+aristo = [ i for i in aristo[n:m+1] if i != 0 ]
+
+for i in aristo:
+    if i == 0 or i == 1:
+        continue
+    print(i)
+    
