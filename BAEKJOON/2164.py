49691e4d3b8b79d6d56f715ad71d7612678cb025
deeptoo2 <deeptoo2@naver.com>
2022-12-28 10:55:07 +0900

    S4

---
+++ BAEKJOON/2164.py
@@ -0,0 +1,12 @@
+import collections
+N = int(input())
+
+
+cards = collections.deque([i for i in range(1, N+1)])
+
+
+while len(cards) > 1:
+    cards.popleft()
+    cards.append(cards.popleft())
+
+print(cards[0])
